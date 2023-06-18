65cfa1e392b1ff507788553ca1d6b420a0f3f9ac
Julian andres fajardo <julian10fajardo@gmail.com>
2023-06-18 01:35:55 -0500

    models biblioteca, api

---
+++ Backend/biblioteca/api/booksViewSet.py
@@ -0,0 +1,6 @@
+from rest_framework import viewsets
+from rest_framework import permissions
+
+# Create your views here.
+""" class BooksViewSet(viewsets.ModelViewSet):
+    queryset = Books.objects.all() """
---
+++ Backend/biblioteca/api/routerBiblioteca.py
@@ -0,0 +1,12 @@
+from rest_framework import routers
+""" from api.views import  """
+
+# Routers provide an easy way of automatically determining the URL conf.
+router_biblioteca = routers.DefaultRouter()
+
+# RegisterView Users
+""" router_biblioteca.register(
+    prefix='routers biblioteca',
+    basename='biblioteca',
+    viewset=views
+) """
